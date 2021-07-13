6b2ab0878ccbbadd2ad0f12ac5026e5f7fad1fe8
Brian Smith <brian.bubbam@gmail.com>
2021-07-13 17:02:01 -0500

    commit tree 6369d39b2a460f842261b320711aecb40a896941

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
+require "pry"
+
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+    # each_with_index argument use the element argument and then the index number
+    # printing on seperate lines and adding + 1 to make 1,2,3 before each element
+  end
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer)# code an argument here
   # Your code here
+  array = []
+  #
+  array = planeteer.map do |word|
+    word.capitalize + "!"
+  end
+  array
+
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(check_cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  check_cheeses.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
